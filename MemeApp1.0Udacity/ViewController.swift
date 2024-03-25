82bf19af23026aef60a564936081388bc1f8894b
Ivan Galarza <id.galarza.sa@gmail.com>
2024-03-25 12:42:24 -0400

    Initial Commit

---
+++ MemeApp1.0Udacity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeApp1.0Udacity
+//
+//  Created by Ivan Galarza on 25/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
